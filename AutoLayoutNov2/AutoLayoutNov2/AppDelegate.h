599f30172c852a021f26fbbd0da0034c24cc9c40
htakashima <taka@TAKASHIMA-no-MacBook-Pro.local>
2015-11-02 15:27:08 +0900

    AutoLayout3 made

---
+++ AutoLayoutNov2/AutoLayoutNov2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AutoLayoutNov2
+//
+//  Created by htakashima on 2015/11/02.
+//  Copyright © 2015年 TAKASHIMA HIROSHI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AutoLayoutNov2/AutoLayoutNov2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutoLayoutNov2
+//
+//  Created by htakashima on 2015/11/02.
+//  Copyright © 2015年 TAKASHIMA HIROSHI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
